34f26b4f2a60eb51787f1c8161e88b70d1adfefe
Fuatari <sam.western@gmail.com>
2018-02-06 11:33:00 +0000

    moved logic into service

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Service service = new Service();
-		
-		Movie m1 = new Movie("Mayhem", "Action", "18");
-		Movie m2 = new Movie("A Silent Voice", "Anime", "3");
-		Movie m3 = new Movie("Alien", "Horror", "12");
-		
-		service.addToMap(1, m1);
-		service.addToMap(2, m2);
-		service.addToMap(3, m3);
-		
+
+		service.generateTestMap();
 		service.printMap();
-		
 	}
 
 }
