1aa40427308fac53aa215fecc128fbae7d1faa73
Fuatari <sam.western@gmail.com>
2018-02-06 15:13:08 +0000

    Added junit test

---
+++ src/MainTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	Service service = new Service();
+
+	@Test
+	void verifyGenreCount() {
+		service.generateTestMap();
+		int expectedCount = 1;
+		int actualCount = service.countGenre();
+		System.out.println(expectedCount + ", " + actualCount);
+		assertEquals(actualCount, expectedCount);
+	}
+
+}
